5d53325f0f1a1f5f595cc2933f5e4c3d24035c2d
vadim <tutumbul@gmail.com>
2021-04-10 00:20:18 +0300

    jq is optional

--- emailleaktest.sh
+++ emailleaktest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 RED='\033[0;31m'
 BOLD='\033[1m'
@@ -27,6 +27,7 @@ function program_exit {
     increment_error_code
 }
 
+
 function check_internet_connection {
     curl --silent --head  --request GET "https://${api_domain}" | grep "200 OK" > /dev/null
     if [ $? -ne 0 ]; then
@@ -36,43 +37,90 @@ function check_internet_connection {
     increment_error_code
 }
 
+function print_servers {
+
+    if (( $jq_exists )); then
+
+        echo ${result} | \
+            jq  --monochrome-output \
+            --raw-output \
+            ".[] | select(.type == \"${1}\") | \"\(.ip)\(if .country_name != \"\" and  .country_name != false then \" [\(.country_name)\(if .asn != \"\" and .asn != false then \" \(.asn)\" else \"\" end)]\" else \"\" end)\""
+
+    else
+
+        while IFS= read -r line; do
+            if [[ "$line" != *${1} ]]; then
+                continue
+            fi
+
+            ip=$(echo $line | cut -d'|' -f 1)
+            code=$(echo $line | cut -d'|' -f 2)
+            country=$(echo $line | cut -d'|' -f 3)
+            asn=$(echo $line | cut -d'|' -f 4)
+
+            if [ -z "${ip// }" ]; then
+                 continue
+            fi
+
+            if [ -z "${country// }" ]; then
+                 echo "$ip"
+            else
+                 if [ -z "${asn// }" ]; then
+                     echo "$ip [$country]"
+                 else
+                     echo "$ip [$country, $asn]"
+                 fi
+            fi
+        done <<< "$result"
+
+    fi
+}
+
 program_exit curl
 program_exit ping
-program_exit jq
 program_exit mail
 check_internet_connection
 
-id=$(shuf -i 1000000-9999999 -n 1)
+if command -v jq &> /dev/null; then
+    jq_exists=1
+else
+    jq_exists=0
+fi
 
-result=$(curl --silent "https://${api_domain}/email-leak-test/test/${id}?json")
+if hash shuf 2>/dev/null; then
+    id=$(shuf -i 1000000-9999999 -n 1)
+else
+    id=$(jot -w %i -r 1 1000000 9999999)
+fi
+
+if (( $jq_exists )); then
+    format="json"
+else
+    format="txt"
+fi
+
+result=$(curl --silent "https://${api_domain}/email-leak-test/test/${id}?${format}")
 
 mail -s "Test" ${id}@bash.ws  < /dev/null > /dev/null
 
 for (( ; ; ))
 do
-    result=$(curl --silent "https://${api_domain}/email-leak-test/test/${id}?json")
+    result=$(curl --silent "https://${api_domain}/email-leak-test/test/${id}?${format}")
 
-    is_done=$(jq 'map(select(.type == "done"))' <<< ${result} | jq '.[0].done')
+    is_done=$(print_servers "done")
 
-    if [[ "$is_done" == "\"1\"" ]]; then
+    if [[ $is_done == *"1"* ]]; then
 
         break
     fi
 
 done
 
-function print_servers {
-    jq  --monochrome-output \
-        --raw-output        \
-        ".[] | select(.type == \"${1}\") | \"\(.ip)\(if .country_name != \"\" and  .country_name != false then \" [\(.country_name)\(if .asn != \"\" and .asn != false then \" \(.asn)\" else \"\" end)]\" else \"\" end)\"" \
-        <<< ${result}
-}
-
 echo_bold "Your IP:"
 print_servers "ip"
 
 echo ""
-ips_count=$(jq 'map(select(.type == "mail")) | length' <<< ${result})
+ips_count=$(print_servers "mail" | wc -l)
 if [ ${ips_count} -eq "0" ];then
     echo_bold "No IPs found in mail header"
 else
@@ -86,6 +134,6 @@ fi
 
 echo ""
 echo_bold "Conclusion:"
-jq --monochrome-output --raw-output '.[] | select(.type == "conclusion") | .ip' <<< ${result}
+print_servers "conclusion"
 
 exit 0
